bacbf59a5fce0ab873308861c2a7fa2dacf3397e
fatihguner41 <fatihguner7@gmail.com>
2023-11-25 21:28:38 +0300

    Solved the bug about news date's.

--- NewsAppWebsite/Controllers/HomeController.cs
+++ NewsAppWebsite/Controllers/HomeController.cs
@@ -2,12 +2,15 @@
 using FireSharp.Config;
 using FireSharp.Interfaces;
 using FireSharp.Response;
+using Google.Cloud.Firestore;
 using Microsoft.AspNetCore.Mvc;
 using NewsAppWebsite.Interfaces;
 using NewsAppWebsite.Models;
 using Newtonsoft.Json;
 using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Reflection.Emit;
 using System.Security.Cryptography.X509Certificates;
 
@@ -27,7 +30,6 @@ namespace NewsAppWebsite.Controllers
 
         public  IActionResult  IndexAsync()
         {
-
             IndexViewModel indexViewModel = new IndexViewModel("home","home");
             return View(indexViewModel);
         }
@@ -64,6 +66,7 @@ namespace NewsAppWebsite.Controllers
             
         }
 
+       
         private (string ,Dictionary<string, News>) GetNewsFromFirebase(string type, string name, string lastNewsDate, Dictionary<string, News> news,int amount)
         {
             QueryBuilder query = QueryBuilder.New();
@@ -76,10 +79,11 @@ namespace NewsAppWebsite.Controllers
             }
             else
             {
+                lastNewsDate=IncrementDate(lastNewsDate);
                 query = query
-            .OrderBy("haber_date")
-            .EndAt(lastNewsDate)
-            .LimitToLast(amount); // İlk bin haber için
+                .OrderBy("haber_date")
+                .EndAt(lastNewsDate)
+                .LimitToLast(amount); // İlk bin haber için
             }
 
 
@@ -92,31 +96,34 @@ namespace NewsAppWebsite.Controllers
             if (response.Body != "null")
             {
                 newsDict = JsonConvert.DeserializeObject<Dictionary<String, News>>(response.Body.ToString());
-                var selectedNews = newsDict.Values.OrderBy(news => news.haber_date).Reverse();
-
-                lastNewsDate = selectedNews.Last().haber_date;
-
-                filteredNewsDict = selectedNews.ToDictionary(news => news.haber_link);
-                filteredNewsDict = GetFilteredNews(type, name, filteredNewsDict);
-
-
-                // dictionary2 içindeki çiftleri birleşikDictionary'e ekleyin
-                foreach (var pair in filteredNewsDict)
+                if(newsDict != null)
                 {
-                    // Aynı anahtar değerine sahip bir çift dictionary1'de ise silin
-                    if (!news.ContainsKey(pair.Key))
-                    {
+                    var selectedNews = newsDict.Values.OrderBy(news => news.haber_date).Reverse();
 
-                        news[pair.Key] = pair.Value;
+                    lastNewsDate = selectedNews.Last().haber_date;
+
+                    filteredNewsDict = selectedNews.ToDictionary(news => news.haber_link);
+                    filteredNewsDict = GetFilteredNews(type, name, filteredNewsDict);
+
+
+                    // dictionary2 içindeki çiftleri birleşikDictionary'e ekleyin
+                    foreach (var pair in filteredNewsDict)
+                    {
+                        // Aynı anahtar değerine sahip bir çift dictionary1'de ise silin
+                        if (!news.ContainsKey(pair.Key))
+                        {
+
+                            news[pair.Key] = pair.Value;
+                        }
+
                     }
 
+
                 }
-
-
-                return (lastNewsDate,news);
+                
             }
 
-            return ("",null);
+            return (lastNewsDate,news);
         }
         public IActionResult Privacy()
         {
@@ -149,5 +156,77 @@ namespace NewsAppWebsite.Controllers
             }
             return news;
         }
+
+        private string IncrementDate(string date = "")
+        {
+
+            // String'i DateTime nesnesine çevir
+            DateTime tarih;
+            string tarihString = "";
+            if (date != null)
+            {
+
+                date = date.Replace("2023", "23");
+                date.Replace(".", "/");
+            }
+            Console.WriteLine(date);
+            if (DateTime.TryParseExact(date, "yy/MM/dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out tarih))
+            {
+                // Geçerli bir tarih
+                // Tarihi bir saniye ileri al
+                tarih = tarih.AddSeconds(-1);
+
+                // Sonucu yazdır
+                Console.WriteLine("Eski Tarih: " + date);
+                
+                tarihString = tarih.ToString("yy/MM/dd HH:mm:ss");
+                tarihString=tarihString.Replace(".", "/");
+                Console.WriteLine("Yeni Tarih: " + tarihString);
+                return tarihString;
+            }
+            else
+            {
+                // Geçersiz tarih formatı
+                Console.WriteLine("Geçersiz tarih formatı: " + date);
+            }
+            return date;
+        }
+
+
+        void UpdateNewsDates()
+        {
+            string yearPrefix = "2023";
+
+            QueryBuilder query = QueryBuilder.New()
+                .OrderBy("haber_date")
+                .StartAt(yearPrefix)
+                .EndAt(yearPrefix + "\uf8ff");
+
+            FirebaseResponse response = _firebaseService.FirebaseClient.Get("haberler", query);
+
+            if (response.Body != "null")
+            {
+                Dictionary<string, News> newsDict = JsonConvert.DeserializeObject<Dictionary<string, News>>(response.Body.ToString());
+
+                foreach (var newsEntry in newsDict)
+                {
+                    var news = newsEntry.Value;
+                    if (news.haber_date.StartsWith(yearPrefix))
+                    {
+                        string newDate = "23" + news.haber_date.Substring(4);
+                        news.haber_date = newDate;
+
+                        // Haber güncelle
+                        _firebaseService.FirebaseClient.Set($"haberler/{newsEntry.Key}", news);
+                        Console.WriteLine($"Haber güncellendi: {newsEntry.Key} - {news.haber_date}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Düğüm boş.");
+            }
+        }
+
     }
 }
